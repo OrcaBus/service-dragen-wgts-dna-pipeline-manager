39c1bff9f2d5af2cb36c957cb25a2141c3f042f9
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2026-06-30 11:41:38 +1000

    feat(schemas): Implement versioned event schema registration and lookup

--- infrastructure/stage/interfaces.ts
+++ infrastructure/stage/interfaces.ts
@@ -36,7 +36,12 @@ export interface StatelessApplicationStackConfig {
 
 export type WorkflowVersionType = '4.4.4' | '4.4.6';
 
+export type PayloadVersionType = '2025.06.04';
+
 export type OraReferenceVersionType = '2.7.0';
+
+/* Consts of types */
+export const payloadVersionList: PayloadVersionType[] = ['2025.06.04'];
 
 export interface Reference {
   name: string;
